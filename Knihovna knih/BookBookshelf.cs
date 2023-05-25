f6858b31ceb707997b3eabb40f391a46448d2946
Daniel Prchlík <daniel.prchlik@educhem.cz>
2023-05-25 20:44:26 +0200

    V.1.0.

--- Knihovna knih/BookBookshelf.cs
+++ Knihovna knih/BookBookshelf.cs
@@ -11,30 +11,131 @@ namespace Knihovna_knih
         List<InfoBook> bookshelf = new List<InfoBook>();
         public void CreateNewBook()
         {
+            Console.Clear();
             InfoBook info = new InfoBook();
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("       ADD NEW BOOK");
             Console.WriteLine("--------------------------");
+            Console.ResetColor();
             try
             {
                 Console.Write("Book name: ");
-                info.Name = Console.ReadLine();
-                Console.WriteLine("Author: ");
-                info.Author = Console.ReadLine();
+                info.Name = Console.ReadLine().ToLower();
+                Console.Write("Author: ");
+                info.Author = Console.ReadLine().ToLower();
                 Console.Write("Release date: ");
-                info.Year = int.Parse(Console.ReadLine());
+                info.Year = int.Parse(Console.ReadLine().ToLower());
                 bookshelf.Add(info);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
-            }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.ResetColor();
+            }           
         }
         public void ShowEveryBook()
         {
-            foreach(InfoBook i in bookshelf)
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("               BOOKSHELF");
+            Console.WriteLine("----------------------------------------");
+            Console.ResetColor();
+            if (bookshelf.Count > 0)
             {
-                Console.WriteLine($"{i.Name} - {i.Author} - {i.Year}");
+                foreach (InfoBook i in bookshelf)
+                {
+                    Console.WriteLine($"Book's name: {i.Name}   ,   Author's name: {i.Author}   ,   Year of release: {i.Year}");
+                }
             }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("         **Bookshelf is empty**");
+                Console.ResetColor ();
+            }          
+        }
+        public void AuthorBook()
+        {
+            InfoBook info = new InfoBook();
+            Console.Clear();
+            if(bookshelf.Count > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("         Search for a book by Author's name");
+                Console.WriteLine("----------------------------------------------------");
+                Console.ResetColor();
+                try
+                {
+                    Console.Write("Enter author's name: ");
+                    string AuthorsName = Console.ReadLine().ToLower();
+                    InfoBook Authors = bookshelf.Find(info => info.Author.Contains(AuthorsName));
+                    if (Authors != null)
+                    {
+                        Console.WriteLine($"Book's name: {Authors.Name}");
+                        Console.WriteLine("--------------------------");
+                        Console.WriteLine($"Year of release: {Authors.Year}");
+                        Console.WriteLine($"Name of author: {Authors.Author}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("**Author wasn't found**");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error: {ex.Message}");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("  **Enter book first**");
+                Console.ResetColor();
+            }       
+        }
+        public void YearBook()
+        {
+            InfoBook info = new InfoBook();
+            Console.Clear();
+            if (bookshelf.Count > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("            Search for a book by year");
+                Console.WriteLine("----------------------------------------------------");
+                Console.ResetColor();
+                try
+                {
+                    Console.Write("Enter year of release: ");
+                    int year = int.Parse(Console.ReadLine());
+                    InfoBook foundYear = bookshelf.Find(info => info.Year == year);
+                    if (foundYear != null)
+                    {
+                        Console.WriteLine($"Book's name: {foundYear.Name}");
+                        Console.WriteLine("--------------------------");
+                        Console.WriteLine($"Year of release: {foundYear.Year}");
+                        Console.WriteLine($"Name of author: {foundYear.Author}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("**Book wasn't found**");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error: {ex.Message}");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("  **Enter book first**");
+                Console.ResetColor();
+            }           
         }
     }
 }
--- Knihovna knih/Program.cs
+++ Knihovna knih/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using Knihovna_knih;
+BookBookshelf bookBookshelf = new BookBookshelf();
+bool loop = true;
+while (loop)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("------------------------");
+    Console.WriteLine("          MENU");
+    Console.WriteLine("------------------------");
+    Console.ResetColor();
+    Console.WriteLine("1) Add book");
+    Console.WriteLine("2) Show every saved book");
+    Console.WriteLine("3) Search for a book (by Author)");
+    Console.WriteLine("4) Search for a book (by Year)");
+    Console.WriteLine("5) Close program");
+    ConsoleKeyInfo key = Console.ReadKey(true);
+    switch (key.Key)
+    {
+        case ConsoleKey.D1:
+            bookBookshelf.CreateNewBook();
+            break;
+        case ConsoleKey.D2:
+            bookBookshelf.ShowEveryBook();
+            break;
+        case ConsoleKey.D3:
+            bookBookshelf.AuthorBook();
+            break;
+        case ConsoleKey.D4:
+            bookBookshelf.YearBook();
+            break;
+        case ConsoleKey.D5:
+            loop = false;
+            break;
+        default:
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("**Error with selection**");
+            Console.ResetColor();
+            break;
+    }
+}
+
